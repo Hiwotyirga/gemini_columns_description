b4fa4062e956a6c2469e7894acda8e82fb7ad536
Hiwot yirga <hiwotyirga30@gmail.com>
2025-03-17 19:36:56 +0300

    API KEY

--- llama/utils.py
+++ llama/utils.py
@@ -1,8 +1,12 @@
 import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 
-GEMINI_API_KEY = "AIzaSyDBzPbHUv3t95KuuBEWIwPIaGbaBglzBPU"
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
 genai.configure(api_key=GEMINI_API_KEY)
-
 
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
